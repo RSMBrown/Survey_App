fa2e168ac830d4fb74f9050fc1af16a4bf43370a
RSMBrown <rsmbrown92@gmail.com>
2022-10-12 16:12:19 +0200

    Final

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  enum roles: { admin: 0, desginer: 1, taker: 2 }
+  enum roles: { admin: 0, designer: 1, taker: 2 }
 end
